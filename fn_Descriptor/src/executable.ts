f88bbde7295974e26800fce48e7b0f944971d2b3
i.a.grekhov <iagrekhov.fronted@gmail.com>
2025-05-17 00:16:59 +0500

    Add descriptor fn

---
+++ fn_Descriptor/src/executable.ts
@@ -0,0 +1,23 @@
+const descriptor = {};
+
+Object.defineProperty(descriptor, "testDescriptor", {
+  value: "valueFieldDescriptor",
+});
+
+// console.log(descriptor);
+// console.log(descriptor?.["testDescriptor"]);
+// console.log(Object.getOwnPropertyDescriptor(descriptor, "testDescriptor"));
+// console.log(descriptor.hasOwnProperty("testDescriptor"));
+
+const descriptionWithGetter = {};
+
+Object.defineProperty(descriptionWithGetter, "descrip", {
+  get: () => {
+    const testString = "Test string";
+    console.log("Получение свойства descrip");
+    return testString;
+  },
+  enumerable: true,
+});
+
+console.log(descriptionWithGetter?.["descrip"]);
